56987c66ed2d2bc6fbf177b2a1d2810a0fc55d44
AtlasBright <brightjackscp@gmail.com>
2026-01-16 09:47:26 +0300

    Add files via upload

---
+++ HW 1.2.1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    int num;
+    std::cout << "Enter a number:" << std::endl;
+    std:: cin >> num;
+    std::cout << "your number: \n"<< num << std::endl;
+    return 0;
+}
