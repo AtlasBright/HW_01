6b8e4d3ed3ebd8c37235e3d3fe32f6ef08bf15f8
AtlasBright <brightjackscp@gmail.com>
2026-01-16 16:37:44 +0300

    Update main.cpp

--- HW 1.2.1/main.cpp
+++ HW 1.2.1/main.cpp
@@ -3,7 +3,7 @@
 int main(){
     int num;
     std::cout << "Enter a number:" << std::endl;
-    std:: cin >> num;
+    std::cin >> num;
     std::cout << "your number: \n"<< num << std::endl;
     return 0;
 }
