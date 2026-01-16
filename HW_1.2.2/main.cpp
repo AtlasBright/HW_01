c04d95a22789de3c0b40a786903017ab86061b53
AtlasBright <brightjackscp@gmail.com>
2026-01-16 14:55:34 +0300

    Add files via upload

---
+++ HW_1.2.2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string.h>
+
+
+int main(){
+    std::string word;
+    std::cout << "enter a word:" << std::endl;
+    std::cin >> word;
+    std::cout << "you word:" << std::endl;
+    std::cout << word;
+    return 0;
+}
